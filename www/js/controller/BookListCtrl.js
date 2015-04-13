2cd6a373aefaf5331c99aeb3058894bd1e4d96b2
Marcus Ross <marcus.ross@zahlenhelfer.de>
2015-04-13 22:24:22 +0200

    added pull-to-refresh on BookList

--- www/js/controller/BookListCtrl.js
+++ www/js/controller/BookListCtrl.js
@@ -9,6 +9,17 @@ angular.module('bookMonkeyMobile')
       $log.log('An error occurred!', error);
     });
 
+    $scope.doRefresh = function () {
+      BookDataService.getBooks().then(function (res) {
+        $scope.books = res.data;
+      }, function (error) {
+        $log.log('An error occurred!', error);
+      }).finally(function () {
+        // Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    };
+
     $scope.remove = function (book) {
       BookDataService.remove(book).then(function (res) {
         $scope.books.slice(book);
