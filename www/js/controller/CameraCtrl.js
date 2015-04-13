9703a9d7e11209023f07a4252dd0445305689177
Marcus Ross <marcus.ross@zahlenhelfer.de>
2015-04-13 23:13:14 +0200

    started to polish the cover-gallery

--- www/js/controller/CameraCtrl.js
+++ www/js/controller/CameraCtrl.js
@@ -2,77 +2,85 @@
 angular.module('bookMonkeyMobile')
   .controller('cameraCtrl', function ($scope, $log, $cordovaCamera, $cordovaFile) {
 // 1
-  $scope.images = [];
+    $scope.images = [];
 
-  $scope.addImage = function () {
-    // 2
-    var options = {
-      destinationType: Camera.DestinationType.FILE_URI,
-      sourceType: Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-      allowEdit: false,
-      encodingType: Camera.EncodingType.JPEG,
-      popoverOptions: CameraPopoverOptions,
+    $scope.addImage = function () {
+      // 2
+      var options = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        popoverOptions: CameraPopoverOptions,
+      };
+
+      // 3
+      $cordovaCamera.getPicture(options).then(function (imageData) {
+
+        // 4
+        onImageSuccess(imageData);
+
+        function onImageSuccess(fileURI) {
+          createFileEntry(fileURI);
+        }
+
+        function createFileEntry(fileURI) {
+          window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+        }
+
+        // 5
+        function copyFile(fileEntry) {
+          var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+          var newName = makeid() + name;
+
+          window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (fileSystem2) {
+              fileEntry.copyTo(
+                fileSystem2,
+                newName,
+                onCopySuccess,
+                fail
+              );
+            },
+            fail);
+        }
+
+        // 6
+        function onCopySuccess(entry) {
+          $scope.$apply(function () {
+            $scope.images.push(entry.nativeURL);
+          });
+        }
+
+        function fail(error) {
+          $log.log("fail: " + error.code);
+        }
+
+        function makeid() {
+          var text = "";
+          var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+          for (var i = 0; i < 5; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+          }
+          return text;
+        }
+
+      }, function (err) {
+        $log.log(err);
+      });
+    }
+
+    $scope.urlForImage = function (imageName) {
+      var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+      var trueOrigin = cordova.file.dataDirectory + name;
+      return trueOrigin;
     };
 
-    // 3
-    $cordovaCamera.getPicture(options).then(function (imageData) {
-
-      // 4
-      onImageSuccess(imageData);
-
-      function onImageSuccess(fileURI) {
-        createFileEntry(fileURI);
+    $scope.images = [];
+    $scope.loadImages = function () {
+      for (var i = 0; i < 100; i++) {
+        $scope.images.push({id: i, src: "http://placehold.it/50x50"});
       }
+    };
 
-      function createFileEntry(fileURI) {
-        window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-      }
-
-      // 5
-      function copyFile(fileEntry) {
-        var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-        var newName = makeid() + name;
-
-        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (fileSystem2) {
-            fileEntry.copyTo(
-              fileSystem2,
-              newName,
-              onCopySuccess,
-              fail
-            );
-          },
-          fail);
-      }
-
-      // 6
-      function onCopySuccess(entry) {
-        $scope.$apply(function () {
-          $scope.images.push(entry.nativeURL);
-        });
-      }
-
-      function fail(error) {
-        $log.log("fail: " + error.code);
-      }
-
-      function makeid() {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for (var i = 0; i < 5; i++) {
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-      }
-
-    }, function (err) {
-      $log.log(err);
-    });
-  }
-
-  $scope.urlForImage = function (imageName) {
-    var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-    var trueOrigin = cordova.file.dataDirectory + name;
-    return trueOrigin;
-  }
-});
+  });
